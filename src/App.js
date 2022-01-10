dada31093d61b5e2037b5a6b9625771ce6c42259
TG2408 <tanmaygupta2408@gmail.com>
2022-01-10 11:05:32 +0530

    installed specific bootstrap version containin jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Jumbotron } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-
+      <Jumbotron>
+        <h1>Admin Dashboard</h1>
+      </Jumbotron>
     </div>
   );
 }
