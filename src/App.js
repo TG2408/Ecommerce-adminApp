a1f42dbc1a641a4d94f07774ee98bbc3f9576276
TG2408 <tanmaygupta2408@gmail.com>
2022-01-10 14:38:38 +0530

    BrowserRoutes defined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import Layout from './components/Layout';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import Signin from './containers/Signin';
+import Signup from './containers/Signup';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          {/* exact keyword is used for exact match of url other wise react 
+              supply Home component even at /signin or /signup
+              React works on 1st match 1st supply */}
+          <Route path='/' exact component={Home} />             
+          <Route path='/signin' component={Signin} />
+          <Route path='/signup' component={Signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
