58491c611241eb51179080608079f1eaa3c78abd
TG2408 <tanmaygupta2408@gmail.com>
2022-01-10 20:54:36 +0530

    Admin UI design completed

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -1,11 +1,59 @@
 import React from "react";
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import Layout from "../../components/Layout";
+import Input from '../../components/UI/Input';
 
 const Signup = (props) => {
-    return (
-        <>
-          <h1>Signup</h1>
-        </>
-    )
-}
+  return (
+    <Layout>
+      <Container>
+        <Row style={{ marginTop: "50px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Row>
+                <Col md={6}>
+                  <Input 
+                    label='First Name' 
+                    type="text" 
+                    placeholder="firstname" 
+                    value=""
+                    onChange={() => {}}
+                  />
+                </Col>
+                <Col md={6}>
+                  <Input 
+                    label='Last Name' 
+                    type="text" 
+                    placeholder="lastname"
+                    value=""
+                    onChange={() => {}} 
+                  />
+                </Col>
+              </Row>
+              <Input
+                label='Email address' 
+                type="email" 
+                placeholder="Enter email" 
+                errorMessage="We'll never share your email with anyone else."
+                value=""
+                onChange={() => {}}
+              />
+              <Input 
+                label='Password' 
+                type="password" 
+                placeholder="Password"
+                value=""
+                onChange={() => {}} 
+              />
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
 
 export default Signup;
