c4e97abbc16542951f4efa22db42c60f8e468ec8
TG2408 <tanmaygupta2408@gmail.com>
2022-01-10 14:28:28 +0530

    different pages created

---
+++ src/containers/Signin/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signin = (props) => {
+    return (
+        <>
+        
+        </>
+    )
+};
+
+export default Signin;
---
+++ src/containers/Signup/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = (props) => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Signup;
